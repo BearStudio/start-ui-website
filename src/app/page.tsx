f5f894796a1124f948c938c50146f9da598b40d9
Ivan Dalmet <ivan@dalmet.fr>
2023-07-17 11:41:08 +0200

    fix: Run prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { cn } from "@/lib/utils"
-import Link from "next/link"
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -7,59 +7,53 @@ export default function Home() {
       <FixedSidebar />
       <div className="relative flex-auto">
         <main className="space-y-20 py-20 sm:space-y-32 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
-      <div className="lg:ml-96 lg:flex lg:w-full lg:justify-end lg:pl-32">
-        <div
-          className={cn(
-            'mx-auto max-w-lg lg:mx-0 lg:w-0 lg:max-w-xl lg:flex-auto',
-
-          )}
-        >
-          <h2 className="text-4xl font-bold">Hello</h2>
-        </div>
-      </div>
-    </div>
+          <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
+            <div className="lg:ml-96 lg:flex lg:w-full lg:justify-end lg:pl-32">
+              <div
+                className={cn(
+                  'mx-auto max-w-lg lg:mx-0 lg:w-0 lg:max-w-xl lg:flex-auto'
+                )}
+              >
+                <h2 className="text-4xl font-bold">Hello</h2>
+              </div>
+            </div>
+          </div>
         </main>
       </div>
     </>
-  )
+  );
 }
-
 
 function FixedSidebar() {
   return (
     <div className="relative flex-none overflow-hidden px-6 lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex lg:px-0 ">
-
       <div className="relative flex w-full lg:pointer-events-auto lg:mr-[calc(max(2rem,50%-38rem)+40rem)] lg:min-w-[32rem] lg:overflow-y-auto lg:overflow-x-hidden lg:pl-[max(4rem,calc(50%-38rem))] bg-gray-950">
         <div className="mx-auto max-w-lg lg:mx-0 lg:flex lg:w-96 lg:max-w-none lg:flex-col lg:before:flex-1 lg:before:pt-6">
           <div className="pb-16 pt-20 sm:pb-20 sm:pt-32 lg:py-20">
             <div className="relative">
-            <>
-      <div>
-        <Link href="/">
-          LOGO
-        </Link>
-      </div>
-      <h1 className="mt-14 text-4xl/tight font-bold text-white">
-       Start UI
-      </h1>
-      <p className="mt-4 text-sm/6 text-gray-300">
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita saepe nam distinctio tenetur ea enim quia eligendi non velit dolorem, tempore voluptatum, architecto unde animi aliquid provident dignissimos deserunt recusandae!
-      </p>
-
-    </>
+              <>
+                <div>
+                  <Link href="/">LOGO</Link>
+                </div>
+                <h1 className="mt-14 text-4xl/tight font-bold text-white">
+                  Start UI
+                </h1>
+                <p className="mt-4 text-sm/6 text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Expedita saepe nam distinctio tenetur ea enim quia eligendi
+                  non velit dolorem, tempore voluptatum, architecto unde animi
+                  aliquid provident dignissimos deserunt recusandae!
+                </p>
+              </>
             </div>
           </div>
           <div className="flex flex-1 items-end justify-center pb-4 lg:justify-start lg:pb-6">
-          <p className="flex items-baseline gap-x-2 text-[0.8125rem]/6 text-gray-500">
-      Brought to you by{' '}
-      <Link href="#">
-        BearStudio
-      </Link>
-    </p>
+            <p className="flex items-baseline gap-x-2 text-[0.8125rem]/6 text-gray-500">
+              Brought to you by <Link href="#">BearStudio</Link>
+            </p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{ts,tsx}',
-	],
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
   },
-}
+};
