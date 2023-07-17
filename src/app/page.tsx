948caa847b8330d66bfc412454045af5a7b8ef27
Ivan Dalmet <ivan@dalmet.fr>
2023-07-17 17:56:05 +0200

    feat: sticker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Logo } from '@/components/Logo';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 import Link from 'next/link';
 import { ReactNode } from 'react';
 
@@ -18,7 +19,7 @@ export default function Home() {
 
 function FixedSidebar() {
   return (
-    <div className="relative z-10 flex flex-1 flex-col overflow-hidden bg-gray-950 lg:fixed lg:bottom-0 lg:left-0 lg:top-0 lg:w-[36rem]">
+    <div className="relative z-20 flex min-h-[90vh] flex-1 flex-col overflow-hidden bg-gray-950 lg:fixed lg:bottom-0 lg:left-0 lg:top-0 lg:w-[36rem]">
       {/* GRADIENT */}
       <div className="gradient-text-rgb absolute left-1/2 top-0 h-[300px] w-[300px] -translate-x-1/2 -translate-y-2/3 opacity-60 blur-[80px] lg:left-auto lg:right-0 lg:h-[500px] lg:w-[500px] lg:translate-x-1/2 lg:blur-[100px]" />
       <div className="gradient-text-rgb absolute bottom-0 left-1/2 h-[300px] w-[300px] -translate-x-1/2 translate-y-2/3 opacity-40 blur-[80px] lg:left-auto lg:right-0 lg:h-[500px] lg:w-[500px] lg:translate-x-1/2 lg:blur-[100px]" />
@@ -56,8 +57,18 @@ function FixedSidebar() {
 
 function PageContainer({ children }: { children: ReactNode }) {
   return (
-    <div className="relative flex flex-1 flex-col lg:pl-[36rem]">
-      {children}
+    <div className="relative z-10 flex flex-1 flex-col lg:pl-[36rem]">
+      <div className="relative overflow-hidden">
+        <Image
+          src="/sticker.png"
+          alt="In Open Source We Trust"
+          width={200}
+          height={200}
+          sizes="200px"
+          className="absolute -bottom-8 -left-8 z-20 w-[200px] rotate-12 transition-transform hover:-translate-y-4 hover:translate-x-4 sm:max-w-[36vh]"
+        />
+        {children}
+      </div>
     </div>
   );
 }
