8da8123221686f17d0dbd17a359fb865f0753001
houssembaltii <houssem@bearstudio.fr>
2024-02-22 15:15:40 +0100

    feat(caroussel): add carroussel

--- src/components/Caroussel.tsx
+++ src/components/Caroussel.tsx
@@ -1,35 +1,91 @@
 'use client';
-import React from 'react';
+import React, {
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
-
+import { EmblaCarouselType } from 'embla-carousel';
 export type CarousselProps = {
   images: Array<string>;
   folder: string;
 };
+type PropType = PropsWithChildren<
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >
+>;
 const Caroussel = (props: CarousselProps) => {
-  const [emblaRef] = useEmblaCarousel();
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
   useEmblaCarousel.globalOptions = { loop: true };
+
+  const scrollTo = useCallback(
+    (index: number) => emblaApi && emblaApi.scrollTo(index),
+    [emblaApi]
+  );
+  const onInit = useCallback((emblaApi: EmblaCarouselType) => {
+    setScrollSnaps(emblaApi.scrollSnapList());
+  }, []);
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onInit(emblaApi);
+    onSelect(emblaApi);
+    emblaApi.on('reInit', onInit);
+    emblaApi.on('reInit', onSelect);
+    emblaApi.on('select', onSelect);
+  }, [emblaApi, onInit, onSelect]);
 
   return (
     <div className="embla" ref={emblaRef}>
       <div className="embla__container">
-        {props.images.map((image, idx) => (
+        {props.images.map((image, index) => (
           <Image
-            key={idx}
+            key={index}
             className={cn('embla__slide rounded-xl')}
-            // alt={conf.description}
             sizes="95vw, (min-width: 640px) 32rem, (min-width: 1024px) 45vw, (min-width: 1280px) 1000px"
             src={`/${props.folder}/${image}`}
+            // src="/CoudeurEnSeine/Coudeur3.jpg"
             width="1000"
             height="500"
-            loading="eager"
-            alt="event"
-          ></Image>
+            alt={image}
+          />
+        ))}
+      </div>
+      <div className="embla__dots">
+        {scrollSnaps.map((_, index) => (
+          <DotButton
+            aria-label={`Image ${index}`}
+            key={index}
+            onClick={() => scrollTo(index)}
+            className={'embla__dot'.concat(
+              index === selectedIndex ? ' embla__dot--selected' : ''
+            )}
+          />
         ))}
       </div>
     </div>
+  );
+};
+
+export const DotButton: React.FC<PropType> = (props) => {
+  const { children, ...restProps } = props;
+
+  return (
+    <button type="button" {...restProps}>
+      {children}
+    </button>
   );
 };
 
