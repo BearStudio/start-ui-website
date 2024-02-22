ede2b9f88256ec597262b3b463d8667224174754
houssembaltii <houssem@bearstudio.fr>
2024-02-22 18:57:11 +0100

    feat(caroussel) : update dots to arrows

--- src/components/Caroussel.tsx
+++ src/components/Caroussel.tsx
@@ -21,31 +21,30 @@ type PropType = PropsWithChildren<
 >;
 const Caroussel = (props: CarousselProps) => {
   const [emblaRef, emblaApi] = useEmblaCarousel();
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
-  useEmblaCarousel.globalOptions = { loop: true };
-
-  const scrollTo = useCallback(
-    (index: number) => emblaApi && emblaApi.scrollTo(index),
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+  /*  useEmblaCarousel.globalOptions = { loop: true }; */
+  const scrollPrev = useCallback(
+    () => emblaApi && emblaApi.scrollPrev(),
     [emblaApi]
   );
-  const onInit = useCallback((emblaApi: EmblaCarouselType) => {
-    setScrollSnaps(emblaApi.scrollSnapList());
-  }, []);
+  const scrollNext = useCallback(
+    () => emblaApi && emblaApi.scrollNext(),
+    [emblaApi]
+  );
 
   const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
-    setSelectedIndex(emblaApi.selectedScrollSnap());
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
   }, []);
 
   useEffect(() => {
     if (!emblaApi) return;
 
-    onInit(emblaApi);
     onSelect(emblaApi);
-    emblaApi.on('reInit', onInit);
     emblaApi.on('reInit', onSelect);
     emblaApi.on('select', onSelect);
-  }, [emblaApi, onInit, onSelect]);
+  }, [emblaApi, onSelect]);
 
   return (
     <div className="embla" ref={emblaRef}>
@@ -54,9 +53,8 @@ const Caroussel = (props: CarousselProps) => {
           <Image
             key={index}
             className={cn('embla__slide rounded-xl object-cover')}
-            sizes="95vw, (min-width: 640px) 32rem, (min-width: 1024px) 45vw, (min-width: 1280px) 1000px"
+            sizes="95vw, (min-width: 640px) 32rem , (min-width: 1024px) 45vw, (min-width: 1280px) 1000px"
             src={`/${props.folder}/${image}`}
-            // src="/CoudeurEnSeine/Coudeur3.jpg"
             width="1000"
             height="500"
             alt={image}
@@ -64,30 +62,50 @@ const Caroussel = (props: CarousselProps) => {
           />
         ))}
       </div>
-      <div className="embla__dots">
-        {scrollSnaps.map((_, index) => (
-          <DotButton
-            aria-label={`Image ${index}`}
-            key={index}
-            onClick={() => scrollTo(index)}
-            className={'embla__dot'.concat(
-              index === selectedIndex ? ' embla__dot--selected' : ''
-            )}
-          />
-        ))}
+      <div className="embla__buttons">
+        <PrevButton onClick={scrollPrev} disabled={prevBtnDisabled} />
+        <NextButton onClick={scrollNext} disabled={nextBtnDisabled} />
       </div>
     </div>
   );
 };
 
-export const DotButton: React.FC<PropType> = (props) => {
+export default Caroussel;
+export const PrevButton: React.FC<PropType> = (props) => {
   const { children, ...restProps } = props;
 
   return (
-    <button type="button" {...restProps}>
+    <button
+      className="embla__button embla__button--prev"
+      type="button"
+      {...restProps}
+    >
+      <svg className="embla__button__svg" viewBox="0 0 532 532">
+        <path
+          fill="currentColor"
+          d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
+        />
+      </svg>
       {children}
     </button>
   );
 };
+export const NextButton: React.FC<PropType> = (props) => {
+  const { children, ...restProps } = props;
 
-export default Caroussel;
+  return (
+    <button
+      className="embla__button embla__button--next"
+      type="button"
+      {...restProps}
+    >
+      <svg className="embla__button__svg" viewBox="0 0 532 532">
+        <path
+          fill="currentColor"
+          d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
+        />
+      </svg>
+      {children}
+    </button>
+  );
+};
