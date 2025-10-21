bad70021803126c632c0ece8367affbde0cdc331
houssembaltii <houssem@bearstudio.fr>
2025-10-21 15:43:06 +0100

    feat: add astr-icon and iconify/lucide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
-
+import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx(), robotsTxt(), sitemap()],
+  integrations: [tailwind(), mdx(), robotsTxt(), sitemap(), icon()],
   site: "https://start-ui.com/",
   adapter: vercel(),
 });
